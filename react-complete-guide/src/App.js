8e381c6e2d68f38b1f1511ff9633d9fcc0f4e8ef
fuzzy95 <asif.eshaq@northsouth.edu>
2020-09-13 18:49:20 +0600

    started dynamic styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium from 'ragit adddium';
 
 
 class App extends Component {
