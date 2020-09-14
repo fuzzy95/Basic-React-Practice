338bf761938a2e535b1e05d8d8b79567e438add7
fuzzy95 <asif.eshaq@northsouth.edu>
2020-09-14 14:44:50 +0600

    started styled-components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
 
 
 class App extends Component {
@@ -55,7 +54,11 @@ class App extends Component {
 			border: '1px solid blue',
 			padding: '8px',
 			cursor: 'pointer',
-			margin: '2px'
+			margin: '2px',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'black'
+			}
 		};
 
 		let persons = null;
@@ -75,6 +78,10 @@ class App extends Component {
 			);
 
 			style.backgroundColor = 'red';
+			style[':hover'] = {
+				backgroundColor: 'salmon',
+				color: 'black'
+			}
 		}
 
 		let classes = [];
@@ -105,7 +112,7 @@ class App extends Component {
 	}
 }
 
-export default Radium(App);
+export default App;
 
 //-----------------Example of React HOOK------------------------
 // const [personsState, setPersonsState] = useState({
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import Radium from 'radium';
-import './Person.css';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div`
+        width: 60%;
+        margin: auto;
+        border: 1px solid #eeeeee;
+        box-shadow: 0 2px 3px #cccccc;
+        padding: 16px;
+        text-align: center;
+
+        @media (min-width: 700px) {
+            width: 450px;
+        }
+`;
 
 const Person = (props) => {
     return (
-        <div className="Person">
+        <StyleDiv>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyleDiv>
     );
 }
 
-export default Radium(Person);
+export default Person;
+
+
+//-------------------Radium Example---------------------
+// const style = {
+//     '@media (min-width: 600px)': {
+//         width: '450px'
+//     }
+// };
